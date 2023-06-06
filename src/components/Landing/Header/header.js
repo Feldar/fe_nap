32fcdae71f5308a8b82b50df409e60ffea2d00a0
alumno <alumno@vm1>
2023-06-06 14:58:15 +0200

    arreglos en componentes

--- src/components/Landing/Header/header.js
+++ src/components/Landing/Header/header.js
@@ -6,9 +6,9 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header className="header fixed-top" id="header">
     <div className="container">
-      
+
       <ul className="navbar" id="lista">
-      <img src={logo} alt="logo" className="logo"/>
+        <img src={logo} alt="logo" className="logo" />
         <a href="#home">Home</a>
         <a href="#about">About</a>
         <NavLink to="/dashboard/recipes">
